12694b8aef7a2ce48ade2cdff753f4e1cd6e4c67
Nancy Ta <nxm0314@icloud.com>
2020-01-26 22:11:45 -0500

    started this kind of late but at the moment think of questions and finding appropriate images for the trivia game.

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+const Begin = document.getElementById('begin');
+const Trivia = document.getElementById('Trivia');
+const Question = document.getElementById('Question');
+const Img = document.getElementById('Img');
+const choiceA = document.getElementById('A');
+const choiceB = document.getElementById('B');
+const choiceC = document.getElementById('C');
+const choiceD = document.getElementById('D');
+const counter = document.getElementById('counter');
+const timer = document.getElementById('Timer');
+const clock = document.getElementById('clock');
+const progress = document.getElementById('progress');
+const scoreDiv = document.getElementById('scoreContainer');
+
+let question = [{
+    question : "What tv show was aired between 1995-1998 on Kids WB?",
+    imgSrc : "assets/images/animaniacs.gif",
+    choiceA : "wrong",
+    choiceB : "wrong",
+    choiceC : "Correct",
+    choiceD : "wrong",
+    correct : "C"
+},{
+    question : 
+}]
